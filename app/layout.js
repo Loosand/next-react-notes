defc6b2de0426a1bba13e9b9bfed4428a317b9b0
loosand <loosand@163.com>
2024-09-05 19:35:50 +0800

    feat: day3

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,15 @@
 import './style.css'
 import Sidebar from '@/components/Sidebar'
+import Header from '@/components/Header'
 
 export default async function RootLayout({
   children
 }) {
-
   return (
     <html lang="en">
       <body>
         <div className="container">
+          <Header />
           <div className="main">
             <Sidebar />
             <section className="col note-viewer">{children}</section>
